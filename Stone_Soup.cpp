567f7cc73dcd94ee235af748501f7c5e4ccd7124
Benjamin Jones <bjones@odin.cs.csubak.edu>
2022-02-03 12:37:00 -0800

    Moved animals to the zoo, set down header file example.

---
+++ Stone_Soup.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "glue.h"
+using namespace std;
+
+int main() {
+    hello_world();
+    return 0;
+}
---
+++ UI.cpp
@@ -0,0 +1,8 @@
+#include "glue.h"
+#include <iostream>
+
+using namespace std;
+
+void hello_world(){
+    cout << "Hello World";
+}
