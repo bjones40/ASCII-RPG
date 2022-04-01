d5806bfba996df1815454ee4f2cc02871e3af2d7
Matthew Taylor <mtaylor41@csub.edu>
2022-04-01 12:16:05 -0700

    Created main Stone_Soup Makefile and temporary Stone_Soup2.cpp, made a 3 window basic game loop with player movement working and displaying correctly as well as a controls window with strings showing, still need to get combat loop working and displaying properly as well as a log on bottom window

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: mtmaintest
+all: mtmaintest Stone_Soup
 
 mtmaintest: mtmaintest.cpp
 	g++ -g -lncurses mtmaintest.cpp -o mtmaintest
 
+Stone_Soup: Stone_Soup2.cpp UI.cpp Classes.cpp Combat.cpp
+	g++ -g -lncurses Stone_Soup2.cpp UI.cpp Classes.cpp Combat.cpp -o Stone_Soup
+
 clean:
-	rm -f mtmaintest
+	rm -f mtmaintest Stone_Soup
---
+++ Stone_Soup2.cpp
@@ -0,0 +1,100 @@
+
+#include <iostream>
+#include <ncurses.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include "glue.h"
+#include "playermovement.h"
+#include "terrain.h"
+
+using namespace std;
+
+int main(int argc, char ** argv) 
+{
+    //Basic States
+    bool movement_state = 1;
+    bool combat_state = 0;
+
+
+    //Ncurses start
+    initscr();
+    noecho(); //hides key press input
+    cbreak(); //allows exit on ctrl-c
+    curs_set(0);
+
+    //borders for windows
+    int left, right, top, bottom, tlc, trc, blc, brc;
+    left = right = '|';
+    top = bottom = '-';
+    tlc = trc = blc = brc = '+';
+
+    //Ncurses window declaration
+    WINDOW * playwin = newwin(36, 72, 0, 0); //Window for player movement and combat
+    WINDOW * logwin = newwin(12, 72, 36, 0); //Window for exploration and combat logs
+    WINDOW * controlswin = newwin(36, 20, 0, 72); //Window for displaying controls
+
+    int playxMax, playyMax, logxMax, logyMax, controlsxMax, controlsyMax;
+    getmaxyx(playwin, playyMax, playxMax);
+    getmaxyx(logwin, logyMax, logxMax);
+    getmaxyx(controlswin, controlsyMax, controlsxMax);
+
+    //creating window borders
+    wborder(playwin, left, right, top, bottom, tlc, trc, blc, brc);
+    wborder(logwin, left, right, top, bottom, tlc, trc, blc, brc);
+    wborder(controlswin, left, right, top, bottom, tlc, trc, blc, brc);
+    refresh();
+    wrefresh(playwin);
+    wrefresh(logwin);
+    wrefresh(controlswin);
+
+    //displaying strings for controlswin
+    for (int i = 1; i < controlsyMax-3; i++)
+    {
+        mvwaddstr(controlswin, i, 1, "C-Controls Example");
+    }
+    wrefresh(controlswin);
+
+    
+    Terrain * map = new Terrain(playwin);
+    Player * p = new Player(playwin, 1, 1, '@', map);
+    //map->generatetiles();
+
+    Hero player = Hero("man",1,0,5,5,5,5,0,0,0);
+    Monster enemy = Parse_Monster_Tables(player,normal_enemy);
+
+    
+
+    if (movement_state)
+    {
+        do
+        {
+            p->display();
+            wrefresh(playwin);
+
+            if (p->getmv() == 'x')
+            {
+                movement_state = 0;
+                combat_state = 1;
+                refresh();
+            }
+
+        }while(movement_state);
+    }
+    if (combat_state)
+    {
+        do
+        {
+            //map->
+            Combat_Loop(player,enemy);
+
+
+        }while(combat_state);
+    }
+
+    //Ncurses end
+    getch();
+    endwin();
+    
+    return 0;
+}
