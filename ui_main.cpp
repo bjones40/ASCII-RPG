e296150a7439c604acd0527a0fe5a85dfd7f226f
Sydney Mayfield-Smith <smayfieldsmi@odin.cs.csubak.edu>
2022-04-20 13:47:54 -0700

    scene class definitions and main test

--- ui_main.cpp
+++ ui_main.cpp
@@ -1,8 +1,5 @@
-#include <ncurses.h>
-#include <panel.h>
-#include <string.h>
-#include <stdio.h>
 #include "glue.h"
+#include "scene.h"
 
 #define WIDTH 30
 #define HEIGHT 10
@@ -19,44 +16,6 @@ char const *list[] = {
 };
 
 int n_list = sizeof(list) / sizeof(char *);
-void print_menu(WINDOW *menu_win, int highlight);
-/*Scene(){
-    startCurses();
-}
-~Scene(){
-    endCurses();
-}*/
-
-void Scene::startCurses(){
-    initscr();
-    raw();
-    keypad(stdscr, TRUE);
-    start_color;
-    clear();
-    noecho();
-    cbreak();
-}
-
-void Scene::endCurses(){
-    endwin();
-}
-
-int Scene::getInput(){
-    return getch();
-}
-
-void Scene::sceneUpdate(){
-    refresh();
-}
-
-int Scene::getSize(){
-    return termSize;
-}
-
-void Scene::setSize(int r, int c){
-    termSize = (stdscr , r, c);
-}
-//
 //void Scene::print_menu(WINDOW *menu_win, int highlight){
 void print_menu(WINDOW *menu_win, int highlight){
 	int x, y, i;	
@@ -76,30 +35,56 @@ void print_menu(WINDOW *menu_win, int highlight){
 	}
 	wrefresh(menu_win);
 }
-       
+
+
+//void draw_borders(WINDOW *screen){
+/*	int x, y, i; getmaxyx(screen, y, x);
+	mvwprintw(screen, 0, 0, "+");
+ 	mvwprintw(screen, y - 1, 0, "+"); 
+	mvwprintw(screen, 0, x - 1, "+"); 
+	mvwprintw(screen, y - 1, x - 1, "+"); 
+	for(i = 1; i < (y - 1); i++) { 
+		mvwprintw(screen, i, 0, "|"); 
+		mvwprintw(screen, i, x - 1, "|"); 
+	} 
+	for(i = 1; i < (x - 1); i++) { 
+		mvwprintw(screen, 0, i, "-"); 
+		mvwprintw(screen, y - 1, i, "-"); 
+	}	
+*/   
+//}
 
 //main 
 int main(){
-    Scene scene;
-    WINDOW *menu_win;
+    myScene::Scene scene;
+    //scene obj
+    WINDOW *menu_win, *map, *msg, *controlswin, *inventorywin, *statwin, *battlewin;
+    //menu obj
     int highlight = 1;
     int selection = 0;
     int s;
+   // int n_list = sizeof(list) / sizeof(char *);
     scene.startCurses();
     scene.setSize(startx, starty);
     
-    menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+    menu_win = newwin(HEIGHT, WIDTH, 80, 24);
+    map = newwin(43, 165, 0, 0);
+    msg = newwin(17, 165, 43, 0);
+    controlswin = newwin(60, 16, 0, 165);
     keypad(menu_win, TRUE);
     mvprintw(12, 40, "Stone Soup");
-    
+    wborder(menu_win, 0, 0, 0, 0, 0, 0, 0, 0);
+    wrefresh(menu_win); 
     curs_set(0);
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 	mvchgat(0, 0, -1, A_BLINK, 1, NULL);
 	scene.sceneUpdate();
-
+	//scene.main_menu(menu_win, highlight);
 	print_menu(menu_win, highlight);
 
 	while(1){
+    //scene.start_screen();
+	//print_menu(menu_win, highlight);
 	s = wgetch(menu_win);
 			switch(s)
 			{	case KEY_UP:
@@ -107,15 +92,37 @@ int main(){
 						highlight = n_list;
 					else
 						--highlight;
+
+			        //scene.main_menu(menu_win, highlight);
+			        print_menu(menu_win, highlight);
 					break;
 				case KEY_DOWN:
 					if(highlight == n_list)
 						highlight = 1;
 					else 
 						++highlight;
+
+			        //scene.main_menu(menu_win, highlight);
+			        print_menu(menu_win, highlight);
 					break;
 				case 10:
-					selection = highlight;
+					s = highlight;
+                    scene.sceneUpdate();
+
+                    wborder(map, 0, 0, 0, 0, 0, 0, 0, 0);
+                    mvwaddstr(map, 1, 1, "Map");
+                    wrefresh(map);
+
+                    wborder(msg, 0, 0, 0, 0, 0, 0, 0, 0);
+                    mvwaddstr(msg, 1, 1, "Message Log"); 
+                    wrefresh(msg);
+                    
+                    wborder(controlswin, 0, 0, 0, 0, 0, 0, 0, 0);
+                    mvwaddstr(controlswin, 1, 1, "Controls");
+                    wrefresh(controlswin);
+                    
+                    keypad(menu_win, FALSE);
+
 					break;
 				default:
 					mvprintw(24, 0, " ", s, s);
@@ -123,10 +130,11 @@ int main(){
 					break;
 			}
             //print menu
-			print_menu(menu_win, highlight);
-			if(selection != 0)	
+			if(selection == 3)	
 				break;
 	}	
+    //delwin(map);
+	//delwin(msg);   
 	scene.sceneUpdate();	
 	scene.endCurses();
 
